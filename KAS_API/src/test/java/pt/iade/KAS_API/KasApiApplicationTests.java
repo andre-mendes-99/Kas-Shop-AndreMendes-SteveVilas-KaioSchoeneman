4ccb02e6340e354929b063e89622972953b80994
andre-mendes-99 <andrezmendes18@gmail.com>
2024-02-27 12:38:52 +0000

    atualização da api para springboot

---
+++ KAS_API/src/test/java/pt/iade/KAS_API/KasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.KAS_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
