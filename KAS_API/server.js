eb1ace559f3a8ceee20983df3102c46faaf1b1db
andre-mendes-99 <andrezmendes18@gmail.com>
2024-04-19 10:50:36 +0100

    add descriptios

--- KAS_API/server.js
+++ KAS_API/server.js
@@ -1,15 +1,35 @@
-import { fastify } from "fastify";
-const server = fastify();
+import { fastify } from "fastify"; //import do fastify(biblioteca do server)
+
+const server = fastify(); // constante do servidor que ao usar o método fastify cria o objeto do servidor 
+
+
+const stack = [] // stack para guardar itens de um post ou put 
+
+//endpoints:
+
+//get de teste 
 server.get('/', () =>{
     return "Bem vindo";
 });
 
+//login - vai ser post porque vou mandar um objeto para o servidor e lá é que vou checar se o login é válido ou não
+server.post('/login', (request, reply) => {
+    const item = request.body.item
+    stack.push(item)
+    reply.send(stack)
+})
 
-//endpoints
-server.get('/turma', () =>{
-    return "Node é fixe";
-});
 
-server.listen({
-    port: 3333
-});
+
+//@Server
+//dizer onde o servidor vai estar "hospedado" em que porta, também mostra erros na consola
+server.listen(5000, (err) => {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    } else {
+      console.log(`Server running, navigate to localhost:5000`)
+    }
+  })
+
+  //executar npm run dev para iniciar o server
