d0211740ad007ff24f071627ddafa2354453a5e2
andre-mendes-99 <andrezmendes18@gmail.com>
2024-06-03 15:46:06 +0100

    config

--- KAS_API/config.js
+++ KAS_API/config.js
@@ -1,9 +1,15 @@
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER=process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Passw0rd'
+const DB_NAME = process.env.DB_NAME || 'kas_db'
+
+
 const config = {
     db: {
-      host: "localhost",
-      user: "root",
-      password: "Passw0rd",
-      database: "kas_db",
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
       connectTimeout: 60000
     },
     listPerPage: 10,
